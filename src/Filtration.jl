97fe5099030bc067a08dcabd1f01bdd5a65fbec2
remydutto <35221423+remydutto@users.noreply.github.com>
2025-02-13 17:09:59 +0100

    foo

--- src/Filtration.jl
+++ src/Filtration.jl
@@ -1,18 +1,7 @@
-"""
-[`Filtration`](@ref) module.
-
-Lists all the imported modules and packages:
-
-$(IMPORTS)
-
-List of all the exported names:
-
-$(EXPORTS)
-"""
-
 module Filtration
 
 # using
+using DocStringExtensions
 using LaTeXStrings
 using Plots
 using ForwardDiff
@@ -28,5 +17,19 @@ include("models/membrane_filtration_model.jl")
 export membrane_filtration_model
 export ismonotonic, isLfunction, isKfunction
 export get_roots
+
+
+
+"""
+[`Filtration`](@ref) module.
+
+Lists all the imported modules and packages:
+
+$(IMPORTS)
+
+List of all the exported names:
+
+$(EXPORTS)
+"""
 
 end
