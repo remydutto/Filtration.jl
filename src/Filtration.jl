a1c536ff48633f3db6cdf1a10261ac00a1af5e23
remydutto <35221423+remydutto@users.noreply.github.com>
2025-02-13 15:50:07 +0100

    Add DocStringExtensions

--- src/Filtration.jl
+++ src/Filtration.jl
@@ -1,3 +1,21 @@
+module Filtration
+greet() = print("Hello World!")
+
+# using
+using DocStringExtensions
+using LaTeXStrings
+using Plots
+using ForwardDiff
+using Symbolics, Nemo
+
+# include 
+include("models/membrane_filtration_model.jl")
+
+# export functions only for user
+export membrane_filtration_model
+export ismonotonic, isLfunction, isKfunction
+export get_roots
+
 """
 [`Filtration`](@ref) module.
 
@@ -9,23 +27,5 @@ List of all the exported names:
 
 $(EXPORTS)
 """
-
-module Filtration
-greet() = print("Hello World!")
-
-# using
-using LaTeXStrings
-using Plots
-using ForwardDiff
-using Symbolics, Nemo
-
-# include 
-
-include("models/membrane_filtration_model.jl")
-
-# export functions only for user
-export membrane_filtration_model
-export ismonotonic, isLfunction, isKfunction
-export get_roots
 
 end
